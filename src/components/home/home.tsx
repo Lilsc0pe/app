9465b27c3f222465f0b277ee7517cd762a91d159
Lilsc0pe <107033524+Lilsc0pe@users.noreply.github.com>
2023-12-06 01:03:08 +0300

    home.tsx css fix

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,29 +1,62 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './home.css'; // Import the CSS file
+import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import "./home.css";
+
+import backgroundImage from "../images/background-img.png";
 
 function Home() {
   return (
-    <div className="container">
-      <div className="navbar">
-        <Link to="/announcements">Announcements</Link>
-        <Link to="/news">News</Link>
-        <Link to="/register" className="right">Registration</Link>
-        <Link to="/login" className="right">Login</Link>
-      </div>
+    <form className="form-home">
+      <header className="header">
+        <div className="logo">AutoScout</div>
+        <nav className="navbar">
+          <ul className="nav-links">
+            <li className="li-center">
+              <Link to="/">Оголошення</Link>
+            </li>
+            <li>
+              <Link to="/news">Новини</Link>
+            </li>
+            <li>
+              <Link to="/login">Вхід</Link>
+            </li>
+            <li>
+              <Link to="/register">Регістрація</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className="container">
+        <div className="block">
+          <img src={backgroundImage} alt="image" />
+          <p>Text-1</p>
+          <p>Text-2</p>
+          <p>Text-3</p>
+        </div>
+        <div className="block">
+          <img src={backgroundImage} alt="image" />
+          <p>Text-1</p>
+          <p>Text-2</p>
+          <p>Text-3</p>
+        </div>
 
-      <div className="main-content">
-        <h2>POPULAR ANNOUNCEMENTS</h2>
-        <div className="announcement"></div>
-        <div className="announcement"></div>
+        <div className="block">
+          <img src={backgroundImage} alt="image" />
+          <p>Text-1</p>
+          <p>Text-2</p>
+          <p>Text-3</p>
+        </div>
+        <div className="block">
+          <img src={backgroundImage} alt="image" />
+          <p>Text-1</p>
+          <p>Text-2</p>
+          <p>Text-3</p>
+        </div>
       </div>
-    </div>
+    </form>
   );
 }
 
 export default Home;
-
-
-
-
-
--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -2,7 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import React, { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
-import "./login.css"
+import "./login.css";
 
 function Login() {
   const navigate = useNavigate();
@@ -17,7 +17,7 @@ function Login() {
   const onLogin = (e: any) => {
     e.preventDefault();
     signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {        
+      .then((userCredential) => {
         const user = userCredential.user;
         navigate("/profile");
         console.log(user);
@@ -31,8 +31,8 @@ function Login() {
 
   return (
     <div>
-      <form>
-        <div>       
+      <form className="form-registration-login-profile">
+        <div>
           <label htmlFor="email-address">Email address</label>
           <input
             id="email-address"
@@ -50,7 +50,7 @@ function Login() {
             <input
               id="password"
               name="password"
-              type={isPasswordVisible ? 'text' : 'password'}
+              type={isPasswordVisible ? "text" : "password"}
               required
               placeholder="Password"
               onChange={(e) => setPassword(e.target.value)}
@@ -64,25 +64,24 @@ function Login() {
               viewBox="0 0 576 512"
               onClick={togglePasswordVisibility}
               style={{
-                opacity: isPasswordVisible ? 1 : 0.3, 
+                opacity: isPasswordVisible ? 1 : 0.3,
               }}
             >
-            <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/>
+              <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z" />
             </svg>
           </div>
-      </div>
+        </div>
 
         <div>
           <button onClick={onLogin}>Login</button>
         </div>
 
         <div className="login-question">Don't have an account yet?</div>
-        <Link className = "nav-page" to="/register">Sign up</Link>
-      {auth.currentUser !== null ? 
-
-      <Link to="/profile">Profile</Link> : <></>}
+        <Link className="nav-page" to="/register">
+          Sign up
+        </Link>
+        {auth.currentUser !== null ? <Link to="/profile">Profile</Link> : <></>}
       </form>
-  
     </div>
   );
 }
