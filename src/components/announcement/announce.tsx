fb71800c99c7293c9223b532eab3a54b9107c4b1
DanialProkopov <enderbluestacks123@gmail.com>
2023-12-07 20:49:45 +0100

    Оголошення

--- src/components/announcement/announce.tsx
+++ src/components/announcement/announce.tsx
@@ -45,19 +45,50 @@ function Announce() {
           </ul>
         </nav>
       </header>
-      <div>
-        {item && (
-          <div className="announce">
-            <img src={item.imageURL} alt="image" />
-            <div className="text-container">
-              <h2>{item.name}</h2>
-              <p>{item.text_1}</p>
-              <p>{item.text_2}</p>
-              <p>{item.text_3}</p>
-            </div>
-          </div>
-        )}
+      <div className="main-content">
+  <div className="filters">
+    {/* Фильтры */}
+    <h3>Фильтры</h3>
+    <div>
+      <label htmlFor="year">Год выпуска:</label>
+      <input type="text" id="year" />
+    </div>
+    <div>
+      <label htmlFor="series">Серия авто:</label>
+      <input type="text" id="series" />
+    </div>
+    <div>
+      <label htmlFor="mileage">Пробег:</label>
+      <input type="text" id="mileage" />
+    </div>
+    <div>
+      <label htmlFor="status">Статус авто:</label>
+      <select id="status">
+        <option value="new">Новый</option>
+        <option value="used">Б/у</option>
+      </select>
+    </div>
+  </div>
+  <div className="popular-offers">
+    {/* Блок с популярными предложениями */}
+    {item && (
+      <div className="announce">
+        <img src={item.imageURL} alt="image" />
+        <div className="text-container">
+          <h2>{item.name}</h2>
+          <p>{item.text_1}</p>
+          <p>{item.text_2}</p>
+          <p>{item.text_3}</p>
+        </div>
       </div>
+    )}
+  </div>
+  <div className="search-bar">
+    {/* Поисковая строка */}
+    <input type="text" placeholder="Поиск..." />
+    <button>Найти</button>
+  </div>
+</div>
     </form>
   );
 }
