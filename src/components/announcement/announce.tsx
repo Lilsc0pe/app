8c51a7733917be8c26bc95c500593d1f4c1e5ae9
DanialProkopov <enderbluestacks123@gmail.com>
2023-12-08 13:02:11 +0100

    Оголошення Бонус

--- src/components/announcement/announce.tsx
+++ src/components/announcement/announce.tsx
@@ -45,36 +45,64 @@ function Announce() {
           </ul>
         </nav>
       </header>
-
       <div className="main-content">
-        <div className="search-bar">
-          {/* Поисковая строка */}
-          <input type="text" placeholder="Поиск..." />
-          <button>Найти</button>
-        </div>
         <div className="filters">
           {/* Фильтры */}
           <h3>Фильтры</h3>
-          <div className="filter-label">
-            <div>
-              <label htmlFor="year">Год выпуска:</label>
-              <input type="text" id="year" />
-            </div>
-            <div>
-              <label htmlFor="series">Серия авто:</label>
-              <input type="text" id="series" />
-            </div>
-            <div>
-              <label htmlFor="mileage">Пробег:</label>
-              <input type="text" id="mileage" />
-            </div>
-            <div>
-              <label htmlFor="status">Статус авто:</label>
-              <select id="status">
-                <option value="new">Новый</option>
-                <option value="used">Б/у</option>
-              </select>
-            </div>
+          <div>
+            <label htmlFor="brand">Марка авто:</label>
+            <input type="text" id="brand" />
+          </div>
+          <div>
+            <label htmlFor="year">Год выпуска авто:</label>
+            <input type="text" id="year" />
+          </div>
+          <div>
+            <label htmlFor="status">Статус авто:</label>
+            <select id="status">
+              <option value="new">Новый</option>
+              <option value="used">Б/у</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="mileageFrom">Пробег от:</label>
+            <input type="text" id="mileageFrom" placeholder="От" />
+          </div>
+          <div>
+            <label htmlFor="mileageTo">Пробег до:</label>
+            <input type="text" id="mileageTo" placeholder="До" />
+          </div>
+          <div>
+            <label htmlFor="fuelType">Тип топлива:</label>
+            <select id="fuelType">
+              <option value="petrol">Бензин</option>
+              <option value="diesel">Дизель</option>
+              {/* Другие варианты топлива */}
+            </select>
+          </div>
+          <div>
+            <label htmlFor="seats">Количество мест:</label>
+            <select id="seats">
+              <option value="2">2</option>
+              <option value="4">4</option>
+              {/* Другие варианты количества мест */}
+            </select>
+          </div>
+          <div>
+            <label htmlFor="engineVolumeFrom">Объем двигателя от:</label>
+            <input type="text" id="engineVolumeFrom" placeholder="От" />
+          </div>
+          <div>
+            <label htmlFor="engineVolumeTo">Объем двигателя до:</label>
+            <input type="text" id="engineVolumeTo" placeholder="До" />
+          </div>
+          <div>
+            <label htmlFor="region">Регион авто:</label>
+            <select id="region">
+              <option value="north">Север</option>
+              <option value="south">Юг</option>
+              {/* Другие варианты регионов */}
+            </select>
           </div>
         </div>
         <div className="popular-offers">
@@ -90,6 +118,11 @@ function Announce() {
               </div>
             </div>
           )}
+        </div>
+        <div className="search-bar">
+          {/* Поисковая строка */}
+          <input type="text" placeholder="Поиск..." />
+          <button>Найти</button>
         </div>
       </div>
     </form>
